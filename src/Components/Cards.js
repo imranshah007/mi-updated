4716cb58035b3bcd21ef924df632457efca191d5
Imran shah <94097713+imranshah007@users.noreply.github.com>
2023-01-28 22:50:46 +0530

    Update Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../App.css";
 
-const nameCheapUrl =
-  "https://www.namecheap.com/domains/registration/results/?domain=";
+const Url ="https://www.namecheap.com/domains/registration/results/?domain=";
 const Cards = ({ RandomName }) => {
   return (
     <a
       target="_blank"
       rel="noreferrer"
       className="link"
-      href={`${nameCheapUrl}${RandomName}`}
+      href={`${Url}${RandomName}`}
     >
       <div className="card-container">
         <p className="para">{RandomName}</p>
